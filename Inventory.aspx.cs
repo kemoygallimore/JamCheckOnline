2a798c8d8c8cc26a2d538a42b7e21c08a0f8721d
kemoygallimore <100480863+kemoygallimore@users.noreply.github.com>
2023-04-17 13:37:08 -0500

    updated the inventory system

--- Inventory.aspx.cs
+++ Inventory.aspx.cs
@@ -3,52 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace MyfirstWebApplication1
 {
-    public partial class WebForm4 : System.Web.UI.Page
+    public partial class Inventory : System.Web.UI.Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-            //GridView1.
+            GridView1.DataKeyNames = new string[] { "id", "Mileage","Seating", "ChassisNo", "VIN", "ImportFrom", "Importer", "ImportDate", "Report_name", "Report_Ext" };
             resultspanel.Visible = false;
+            
         }
+    
 
         protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
-        }
-
-        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
-        {
-            /*var select = new baseclass();
-            select.id = GridView1.SelectedIndex;
-            select.mfr = GridView1.Columns[1].ToString();
-
-            Details detailsform = new Details();*/
-
+            var reportcell = GridView1.SelectedDataKey["Report_name"].ToString();
+            if (GridView1.SelectedDataKey["Report_name"].ToString() == "" || GridView1.SelectedDataKey["Report_name"].ToString()==null)
+                No.Checked= true;
+            else
+                Yes.Checked= true;
             
+            Manufacturerlbl.Text = GridView1.SelectedRow.Cells[2].Text;
+            Yearlbl.Text = GridView1.SelectedRow.Cells[7].Text;
+            Modellbl.Text = GridView1.SelectedRow.Cells[3].Text;
+            BodyTypelbl.Text = GridView1.SelectedRow.Cells[11].Text;
+            Transmissionlbl.Text = GridView1.SelectedRow.Cells[12].Text;
+            Fuellbl.Text = GridView1.SelectedRow.Cells[4].Text;
+            Seatinglbl.Text = GridView1.SelectedDataKey["Seating"].ToString();
+            Mileagelbl.Text = GridView1.SelectedDataKey["Mileage"].ToString();
+            Steeringlbl.Text = GridView1.SelectedRow.Cells[10].Text;
+            VINNumlbl.Text = GridView1.SelectedDataKey["VIN"].ToString();
+            ImportedFromlbl.Text = GridView1.SelectedDataKey["ImportFrom"].ToString();
+            displayimage.ImageUrl = ((Image)GridView1.SelectedRow.Cells[0].FindControl("Image1")).ImageUrl;
             tablepanel.Visible = false;
             resultspanel.Visible = true;
 
-            int i = GridView1.SelectedIndex;
+            //Response.Redirect("ViewCarDetails.aspx?");
 
-            JamCheckEntities jamCheck = new JamCheckEntities();
-            
-
-            maketxtbx.Text = GridView1.SelectedRow.Cells[2].ToString();
-
-
-
-        }
-
-        protected void GridView1_SelectedIndexChanged2(object sender, EventArgs e)
-        {
-            tablepanel.Visible = false;
-            resultspanel.Visible = true;
-            maketxtbx.Text = GridView1.Columns[1].ToString();
         }
     }
 }
--- Inventory.aspx.designer.cs
+++ Inventory.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace MyfirstWebApplication1
 {
 
 
-    public partial class WebForm4
+    public partial class Inventory
     {
 
         /// <summary>
@@ -60,12 +60,219 @@ namespace MyfirstWebApplication1
         protected global::System.Web.UI.WebControls.Image displayimage;
 
         /// <summary>
-        /// maketxtbx control.
+        /// Label1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox maketxtbx;
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Label11 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+
+        /// <summary>
+        /// Label12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label12;
+
+        /// <summary>
+        /// Label13 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label13;
+
+        /// <summary>
+        /// Label14 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label14;
+
+        /// <summary>
+        /// Label15 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label15;
+
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Label6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Yearlbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Yearlbl;
+
+        /// <summary>
+        /// Manufacturerlbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Manufacturerlbl;
+
+        /// <summary>
+        /// Modellbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Modellbl;
+
+        /// <summary>
+        /// BodyTypelbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label BodyTypelbl;
+
+        /// <summary>
+        /// Transmissionlbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Transmissionlbl;
+
+        /// <summary>
+        /// Fuellbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Fuellbl;
+
+        /// <summary>
+        /// Seatinglbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Seatinglbl;
+
+        /// <summary>
+        /// Mileagelbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Mileagelbl;
+
+        /// <summary>
+        /// Steeringlbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Steeringlbl;
+
+        /// <summary>
+        /// VINNumlbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label VINNumlbl;
+
+        /// <summary>
+        /// ImportedFromlbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ImportedFromlbl;
+
+        /// <summary>
+        /// Yes control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox Yes;
+
+        /// <summary>
+        /// No control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox No;
     }
 }
